91a6044d3f20864a9c0e9c5ae895bdf18597ca46
Tomasz Orczyk <tzok@poczta.onet.pl>
2018-11-03 11:28:00 +0100

    Initial commit

---
+++ CartMan/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace CartMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tbProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnSplitGo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSplitBrowseFolder = new System.Windows.Forms.Button();
+            this.tbSplitOutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSplitBrowseFile = new System.Windows.Forms.Button();
+            this.tbSplitInputFile = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnMergeBrowseHigh = new System.Windows.Forms.Button();
+            this.tbMergeHighFile = new System.Windows.Forms.TextBox();
+            this.btnMergeGo = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnMergeBrowseOutput = new System.Windows.Forms.Button();
+            this.tbMergeOutputFolder = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMergeBrowseLow = new System.Windows.Forms.Button();
+            this.tbMergeLowFile = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 188);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(414, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(414, 188);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnSplitGo);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnSplitBrowseFolder);
+            this.tabPage1.Controls.Add(this.tbSplitOutputFolder);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnSplitBrowseFile);
+            this.tabPage1.Controls.Add(this.tbSplitInputFile);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(406, 162);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Split L/H";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnSplitGo
+            // 
+            this.btnSplitGo.Location = new System.Drawing.Point(166, 126);
+            this.btnSplitGo.Name = "btnSplitGo";
+            this.btnSplitGo.Size = new System.Drawing.Size(75, 23);
+            this.btnSplitGo.TabIndex = 6;
+            this.btnSplitGo.Text = "GO!";
+            this.btnSplitGo.UseVisualStyleBackColor = true;
+            this.btnSplitGo.Click += new System.EventHandler(this.btnSplitGo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output Folder:";
+            // 
+            // btnSplitBrowseFolder
+            // 
+            this.btnSplitBrowseFolder.Location = new System.Drawing.Point(375, 30);
+            this.btnSplitBrowseFolder.Name = "btnSplitBrowseFolder";
+            this.btnSplitBrowseFolder.Size = new System.Drawing.Size(25, 23);
+            this.btnSplitBrowseFolder.TabIndex = 4;
+            this.btnSplitBrowseFolder.Text = "...";
+            this.btnSplitBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnSplitBrowseFolder.Click += new System.EventHandler(this.btnSplitBrowseFolder_Click);
+            // 
+            // tbSplitOutputFolder
+            // 
+            this.tbSplitOutputFolder.Location = new System.Drawing.Point(86, 32);
+            this.tbSplitOutputFolder.Name = "tbSplitOutputFolder";
+            this.tbSplitOutputFolder.Size = new System.Drawing.Size(283, 20);
+            this.tbSplitOutputFolder.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input File:";
+            // 
+            // btnSplitBrowseFile
+            // 
+            this.btnSplitBrowseFile.Location = new System.Drawing.Point(375, 4);
+            this.btnSplitBrowseFile.Name = "btnSplitBrowseFile";
+            this.btnSplitBrowseFile.Size = new System.Drawing.Size(25, 23);
+            this.btnSplitBrowseFile.TabIndex = 1;
+            this.btnSplitBrowseFile.Text = "...";
+            this.btnSplitBrowseFile.UseVisualStyleBackColor = true;
+            this.btnSplitBrowseFile.Click += new System.EventHandler(this.btnSplitBrowseFile_Click);
+            // 
+            // tbSplitInputFile
+            // 
+            this.tbSplitInputFile.Location = new System.Drawing.Point(65, 6);
+            this.tbSplitInputFile.Name = "tbSplitInputFile";
+            this.tbSplitInputFile.Size = new System.Drawing.Size(304, 20);
+            this.tbSplitInputFile.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.btnMergeBrowseHigh);
+            this.tabPage2.Controls.Add(this.tbMergeHighFile);
+            this.tabPage2.Controls.Add(this.btnMergeGo);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.btnMergeBrowseOutput);
+            this.tabPage2.Controls.Add(this.tbMergeOutputFolder);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.btnMergeBrowseLow);
+            this.tabPage2.Controls.Add(this.tbMergeLowFile);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(406, 162);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Merge L/H";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "HIGH File:";
+            // 
+            // btnMergeBrowseHigh
+            // 
+            this.btnMergeBrowseHigh.Location = new System.Drawing.Point(375, 30);
+            this.btnMergeBrowseHigh.Name = "btnMergeBrowseHigh";
+            this.btnMergeBrowseHigh.Size = new System.Drawing.Size(25, 23);
+            this.btnMergeBrowseHigh.TabIndex = 15;
+            this.btnMergeBrowseHigh.Text = "...";
+            this.btnMergeBrowseHigh.UseVisualStyleBackColor = true;
+            this.btnMergeBrowseHigh.Click += new System.EventHandler(this.btnMergeBrowseHigh_Click);
+            // 
+            // tbMergeHighFile
+            // 
+            this.tbMergeHighFile.Location = new System.Drawing.Point(65, 32);
+            this.tbMergeHighFile.Name = "tbMergeHighFile";
+            this.tbMergeHighFile.Size = new System.Drawing.Size(304, 20);
+            this.tbMergeHighFile.TabIndex = 14;
+            // 
+            // btnMergeGo
+            // 
+            this.btnMergeGo.Location = new System.Drawing.Point(166, 126);
+            this.btnMergeGo.Name = "btnMergeGo";
+            this.btnMergeGo.Size = new System.Drawing.Size(75, 23);
+            this.btnMergeGo.TabIndex = 13;
+            this.btnMergeGo.Text = "GO!";
+            this.btnMergeGo.UseVisualStyleBackColor = true;
+            this.btnMergeGo.Click += new System.EventHandler(this.btnMergeGo_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Output Folder:";
+            // 
+            // btnMergeBrowseOutput
+            // 
+            this.btnMergeBrowseOutput.Location = new System.Drawing.Point(375, 56);
+            this.btnMergeBrowseOutput.Name = "btnMergeBrowseOutput";
+            this.btnMergeBrowseOutput.Size = new System.Drawing.Size(25, 23);
+            this.btnMergeBrowseOutput.TabIndex = 11;
+            this.btnMergeBrowseOutput.Text = "...";
+            this.btnMergeBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnMergeBrowseOutput.Click += new System.EventHandler(this.btnMergeBrowseOutput_Click);
+            // 
+            // tbMergeOutputFolder
+            // 
+            this.tbMergeOutputFolder.Location = new System.Drawing.Point(86, 58);
+            this.tbMergeOutputFolder.Name = "tbMergeOutputFolder";
+            this.tbMergeOutputFolder.Size = new System.Drawing.Size(283, 20);
+            this.tbMergeOutputFolder.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "LOW File:";
+            // 
+            // btnMergeBrowseLow
+            // 
+            this.btnMergeBrowseLow.Location = new System.Drawing.Point(375, 4);
+            this.btnMergeBrowseLow.Name = "btnMergeBrowseLow";
+            this.btnMergeBrowseLow.Size = new System.Drawing.Size(25, 23);
+            this.btnMergeBrowseLow.TabIndex = 8;
+            this.btnMergeBrowseLow.Text = "...";
+            this.btnMergeBrowseLow.UseVisualStyleBackColor = true;
+            this.btnMergeBrowseLow.Click += new System.EventHandler(this.btnMergeBrowseLow_Click);
+            // 
+            // tbMergeLowFile
+            // 
+            this.tbMergeLowFile.Location = new System.Drawing.Point(65, 6);
+            this.tbMergeLowFile.Name = "tbMergeLowFile";
+            this.tbMergeLowFile.Size = new System.Drawing.Size(304, 20);
+            this.tbMergeLowFile.TabIndex = 7;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(406, 162);
+            this.tabPage4.TabIndex = 2;
+            this.tabPage4.Text = "Combine ROMs";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "BIN files (*.bin)|*.bin|All files (*.*)|*.*";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(406, 162);
+            this.tabPage3.TabIndex = 3;
+            this.tabPage3.Text = "Separate ROMs";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 210);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CARTridge MANager";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar tbProgress;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnSplitBrowseFile;
+        private System.Windows.Forms.TextBox tbSplitInputFile;
+        private System.Windows.Forms.Button btnSplitGo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSplitBrowseFolder;
+        private System.Windows.Forms.TextBox tbSplitOutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnMergeBrowseHigh;
+        private System.Windows.Forms.TextBox tbMergeHighFile;
+        private System.Windows.Forms.Button btnMergeGo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnMergeBrowseOutput;
+        private System.Windows.Forms.TextBox tbMergeOutputFolder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMergeBrowseLow;
+        private System.Windows.Forms.TextBox tbMergeLowFile;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ CartMan/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CartMan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnSplitBrowseFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbSplitInputFile.Text = openFileDialog1.FileName;
+                tbSplitOutputFolder.Text = Path.GetDirectoryName(openFileDialog1.FileName);
+                folderBrowserDialog1.SelectedPath = Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnSplitBrowseFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbSplitOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnMergeBrowseLow_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbMergeLowFile.Text = openFileDialog1.FileName;
+                tbMergeOutputFolder.Text = Path.GetDirectoryName(openFileDialog1.FileName);
+                folderBrowserDialog1.SelectedPath = Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnMergeBrowseHigh_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbMergeHighFile.Text = openFileDialog1.FileName;
+                tbMergeOutputFolder.Text = Path.GetDirectoryName(openFileDialog1.FileName);
+                folderBrowserDialog1.SelectedPath = Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnMergeBrowseOutput_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbMergeOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnSplitGo_Click(object sender, EventArgs e)
+        {
+            var HiFile = tbSplitOutputFolder.Text + @"\" + Path.GetFileNameWithoutExtension(tbSplitInputFile.Text) + "_H" + Path.GetExtension(tbSplitInputFile.Text);
+            var LoFile = tbSplitOutputFolder.Text + @"\" + Path.GetFileNameWithoutExtension(tbSplitInputFile.Text) + "_L" + Path.GetExtension(tbSplitInputFile.Text);
+            using (FileStream inputStream = new FileStream(tbSplitInputFile.Text, FileMode.Open))
+            {
+                using (FileStream LoStream = new FileStream(LoFile, FileMode.Create))
+                {
+                    using (FileStream HiStream = new FileStream(HiFile, FileMode.Create))
+                    {
+                        for (long i = 0; i < (inputStream.Length + 1) / 2; i++)
+                        {
+                            LoStream.WriteByte((byte)inputStream.ReadByte());
+                            HiStream.WriteByte((byte)inputStream.ReadByte());
+                            tbProgress.Value = (int)(100 * (i + 1) / LoStream.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnMergeGo_Click(object sender, EventArgs e)
+        {
+            var CName = Path.GetFileNameWithoutExtension(tbMergeLowFile.Text).Remove(Path.GetFileNameWithoutExtension(tbMergeLowFile.Text).Zip(Path.GetFileNameWithoutExtension(tbMergeHighFile.Text), (x, y) => x == y).TakeWhile(b => b).Count());
+            var OutputFile = tbMergeOutputFolder.Text + @"\" + CName + "MGD" + Path.GetExtension(tbMergeLowFile.Text);
+            var LoFile = new FileInfo(tbMergeLowFile.Text);
+            var HiFile = new FileInfo(tbMergeHighFile.Text);
+            if (LoFile.Length == HiFile.Length)
+            {
+                using (FileStream outStream = new FileStream(OutputFile, FileMode.Create))
+                {
+                    using (FileStream LoStream = new FileStream(LoFile.FullName, FileMode.Open))
+                    {
+                        using (FileStream HiStream = new FileStream(HiFile.FullName, FileMode.Open))
+                        {
+                            for (long i = 0; i < LoStream.Length; i++)
+                            {
+                                outStream.WriteByte((byte)LoStream.ReadByte());
+                                outStream.WriteByte((byte)HiStream.ReadByte());
+                                tbProgress.Value = (int)(100 * (i + 1) / LoStream.Length);
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Low/High files merged successfully!", "Job Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbProgress.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("Low/High file sizes don't match!", "Size Mismatch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
